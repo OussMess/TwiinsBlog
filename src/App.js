65921be7b724eecdfd4fd26bb448b98c792c5fca
oussamamessaoudi <Oussm.messaoudi@gmail.com>
2018-10-28 12:37:12 +0000

    adding hashRouter to the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import Home from "./pages/home";
+import Places from "./pages/places";
+import Recommendations from "./pages/recommendations";
+import Activities from "./pages/activities";
+import NotFound from "./pages/notFound";
+
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello world
-            </div>
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/places" component={Places}/>
+                    <Route path="/recommendations" component={Recommendations}/>
+                    <Route path="/activities" component={Activities}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </HashRouter>
         );
     }
 }
