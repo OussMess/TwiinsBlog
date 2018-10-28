0b36de123c5b68edba91104fd8ba45192b7d153b
oussamamessaoudi <Oussm.messaoudi@gmail.com>
2018-10-28 12:36:15 +0000

    Adding Recommendations Page template

---
+++ src/pages/recommendations/component/view.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PageTemplate from "../../../core/template/page";
+
+export const Recommendations = () =>
+    <PageTemplate>
+        <section className="home">
+            <h1>[Recommendations Page]</h1>
+        </section>
+    </PageTemplate>;
