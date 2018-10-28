b0503f2e3ec15b00b0cd301f946884548a95861a
oussamamessaoudi <Oussm.messaoudi@gmail.com>
2018-10-28 12:31:57 +0000

    adding Home page template

---
+++ src/pages/home/component/view.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PageTemplate from "../../../core/template/page/component/view";
+
+export const Home = () =>
+    <PageTemplate>
+        <section className="home">
+            <h1>[Home Page]</h1>
+        </section>
+    </PageTemplate>;
---
+++ src/pages/home/index.js
@@ -0,0 +1,3 @@
+import {Home} from "./component/view";
+
+export default Home;
